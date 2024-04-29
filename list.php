bf0d68b209ba7d34a7e4d71b888239193c882fbe
QueziaKato <130574133+QueziaKato@users.noreply.github.com>
2024-04-29 08:14:59 -0300

    Add files via upload

---
+++ list.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Projeto Gerenciador de Tarefas </title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <script src='main.js'></script>
+</head>
+<body>
+       <div class= "container">
+       <div class= "header">
+      <h1>Gerenciador de Tarefas</h1>
+       </div>
+       <div class="form">
+       <form action="" method="get">
+       <label for="task_name"></label>
+       <input type="text" name= "task_name" placeholder="Nome da Tarefa">
+       <button type="submit">Cadastrar</button>
+       </form>
+        </div>
+        <div class="separator">
+        </div>
+         <div class="list-tasks">
+         <ul>
+         <li>Tarefa 1</li>
+         <li>Tarefa 2</li>
+         <li>Tarefa 3</li>
+         </ul>
+         </div>
+        <div class="footer">
+        <p> Desenvolvido por @nomedil/ </p>
+        </div>
+        </div>
+</body>
+</html>
